81c0b0f1cfc836cef12dcd8cedde81a003ae356b
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-15 14:20:47 +0100

    v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import NavBar from "./components/NavBar";
-import MisCasas from "./pages/MisCasas";
-import Habitaciones from "./components/Habitaciones";
 import Home from "./pages/Home";
-import MisArmarios from "./components/MisArmarios";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -27,8 +24,6 @@ function App() {
           <Routes>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/" element={<LogIn />}></Route>
-            <Route path="/misarmarios" element={<MisArmarios />}></Route>
-            <Route path="/miscasas" element={<MisCasas />}></Route>
           </Routes>
         </Router>
       </div>
