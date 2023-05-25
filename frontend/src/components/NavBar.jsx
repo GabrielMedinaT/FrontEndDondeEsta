2fc04a4cbd8e63c45e46164d6418bf702f25705f
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-25 17:10:22 +0200

    v1

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -37,7 +37,15 @@ const NavBar = () => {
   const [error, setError] = useState('');
   const [loadingCosas, setLoadingCosas] = useState(false);
   const [cosas, setCosas] = useState([]);
- 
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsExpanded(true);
+  };
+  const handleMouseLeave = () => {
+    setIsExpanded(false);
+  };
+
   useEffect(() => {
     localStorage.setItem("darkMode", darkmode);
   }, [darkmode]);
@@ -252,101 +260,9 @@ const NavBar = () => {
     <div className="NavBarSuperior">
   {isLoggedIn ? (
     <>
-      <div className={darkmode ? "lateral-Dark" : "lateral"}> 
-      <div className="lateral-ext">
-      <div className="CambiarTema">
-          <label htmlFor="theme" className="theme">
-            <span className="theme__toggle-wrap">
-              <input
-                id="theme"
-                className="theme__toggle"
-                type="checkbox"
-                role="switch"
-                name="theme"
-                value="dark"
-                checked={darkmode}
-                onChange={toggleDarkMode}
-              />
-              <span className="theme__icon">
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-              </span>
-            </span>
-          </label>
-        </div>
-      <button
-            className={`VerUOcultar ${resetAnimation ? "reset-animation" : ""}`}
-            onClick={() => casas()}
-          >
-             {nombreUsuario}
-          </button>
-          <button
-            onClick={mostrarBuscadorFuncion}
-            className="BuscarVerUOcultar"
-          >
-            Buscar
-          </button>
-          <button
-            onClick={mostrarCasasFuncion}
-            className={`verCasa ${
-              darkmode ? "reset-animation" : ""
-            }`}
-          >Ver casas
-            
-          </button>
-          <button
-            onClick={mostrarHabitacionFuncion}
-            className={`VerUOcultar ${
-              darkmode ? "reset-animation" : ""
-            }`}
-          >
-            {mostrarHabitacion ? "Ocultar Habitaciones" : "Ver Habitaciones"}
-          </button>
-          <button
-            onClick={mostrarArmariosFuncion}
-            className={`VerUOcultar ${
-              darkmode ? "reset-animation" : ""
-            }`}
-          >
-            {mostrarArmarios ? "Ocultar Mueble" : "Ver Mueble"}
-          </button>
-          <button
-            onClick={mostrarCajonesFuncion}
-            className={`VerUOcultar ${
-              darkmode ? "reset-animation" : ""
-            }`}
-          >
-            {mostrarCajones ? "Ocultar Cajones" : "Ver Cajones"}
-          </button>
-          <button
-            onClick={mostrarCosasFuncion}
-            className={`VerUOcultar ${
-              darkmode ? "reset-animation" : ""
-            }`}
-          >
-            {mostrarCosas ? "Ocultar Cosas" : "Ver Cosas"}
-          </button>
-
-          <button
-            className={`btn ${resetAnimation ? "reset-animation" : ""}`}
-            onClick={Logout}
-          >
-            <div className="sign">
-              <svg viewBox="0 0 512 512">
-                <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
-              </svg>
-            </div>
-            <div className="text">Logout</div>
-          </button>
-        
-        </div>     
+      <div className={isExpanded ? "lateral-expandido" : "lateral"}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>    
         <div className="CambiarTema">
           <label htmlFor="theme" className="theme">
             <span className="theme__toggle-wrap">
